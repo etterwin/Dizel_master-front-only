7e715a692ba904caa3cfa01885bbdcd9eec5c513
etterwin <etterwin.yll@gmail.com>
2018-02-22 10:07:46 +0300

    update

--- js/main.js
+++ js/main.js
@@ -21,3 +21,46 @@ $(document).ready(function () {
         return false;
     });
 });
+
+//Это объект Vue, благодаря ему работает карусель
+new Vue({
+    el: '#app', //это область, для которой предначено действо; в элементе указан id контейнера
+    data: {
+        currentView: 'first' //элемент, который будет рендериться по дефолту
+    },
+    components: { //компоненты для шаблонов карусели
+        first: {
+            template: '#first-template'
+        },
+        second: {
+            template: '#second-template'
+        },
+        third: {
+            template: '#third-template'
+        },
+        fourth: {
+            template: '#fourth-template'
+        },
+        fifth: {
+            template: '#fifth-template'
+        },
+        sixth: {
+            template: '#sixth-template'
+        },
+        seventh: {
+            template: '#seventh-template'
+        },
+        eighth: {
+            template: '#eighth-template'
+        },
+        ninth: {
+            template: '#ninth-template'
+        }
+    },
+    methods: { //функция рендеринга
+        switchView: function (view) {
+            this.currentView = view
+        }
+    }
+});
+
